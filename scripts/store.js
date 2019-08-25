b5ff919d1824ee160474945947767685d34e09b0
Quasar Wei <quasar.wei@outlook.com>
2019-08-25 04:17:36 -0700

    styled hamburger menu and ratings

--- scripts/store.js
+++ scripts/store.js
@@ -2,12 +2,12 @@
 
 const STORE = (function() {
 
-  let bookmarks = [
-    {id: cuid(), title: 'Sample 1', link: 'www.google.com', description: 'sample site 1', rating: 5, favicon: '', expanded: false},
-    {id: cuid(), title: 'Sample 2', link: 'www.bing.com', description: 'sample site 2', rating: 2, favicon: '', expanded: true},
-    {id: cuid(), title: 'Sample 3', link: 'www.cryengine.com', description: 'sample site 3', rating: 1, favicon: '', expanded: false},
-    {id: cuid(), title: 'Sample 4', link: 'www.artstation.com', description: 'sample site 4', rating: 3, favicon: '', expanded: false},
-    {id: cuid(), title: 'Sample 5', link: 'www.sketchfab.com', description: 'sample site 5', rating: 4, favicon: '', expanded: false}
+  let bookmarkList = [
+    {id: cuid(), title: 'Sample 1', url: 'www.google.com', desc: 'sample site 1', rating: 5, favicon: '', expanded: false},
+    {id: cuid(), title: 'Sample 2', url: 'www.bing.com', desc: 'sample site 2', rating: 2, favicon: '', expanded: true},
+    {id: cuid(), title: 'Sample 3', url: 'www.cryengine.com', desc: 'sample site 3', rating: 1, favicon: '', expanded: false},
+    {id: cuid(), title: 'Sample 4', url: 'www.artstation.com', desc: 'sample site 4', rating: 3, favicon: '', expanded: false},
+    {id: cuid(), title: 'Sample 5', url: 'www.sketchfab.com', desc: 'sample site 5', rating: 4, favicon: '', expanded: false}
   ];
 
   let adding = false;
@@ -15,15 +15,17 @@ const STORE = (function() {
   let showError = false;
 
   const addBookmark = function() {
-    this.bookmarks.push();
+    this.bookmarkList.push();
   };
 
-  const removeBookmark = function() {
-    this.items = 
+  const removeBookmark = function(id) {
+    this.bookmarkList = this.bookmarkList.filter(item => item.id !== id);
   };
+
+  
 
   return {
-    bookmarks,
+    bookmarkList,
     adding,
     editing,
     showError
